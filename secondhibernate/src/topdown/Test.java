05b00dd3fae2673200582b22b2139c25cd26f9f6
hagenduk <justus-m@hotmail.de>
2014-12-16 13:41:37 +0100

    added hibernate stuff - didnt try if it works, good luck

--- secondhibernate/src/topdown/Test.java
+++ secondhibernate/src/topdown/Test.java
@@ -4,6 +4,9 @@
 package topdown;
 
 import java.math.BigDecimal;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  * @author David
@@ -16,10 +19,13 @@ public class Test {
 	 */
 	public Test() {
 		Catalog c = new Catalog(1, "pizza");
+		createCatalog(c);
 		BigDecimal bd = new BigDecimal(7.50f);
 		CatalogItem ci = new CatalogItem(0, "Mozarella", "Eine Pizza mit frischem Büffel Mozzarella, verfeinert mit Tomaten und Basilikum.", bd, c);
+		createCatalogItem(ci);
 		BigDecimal bd2 = new BigDecimal(8.00f);
 		CatalogItem ci2 = new CatalogItem(0, "Hawaii", "Eine Pizza mit Annanas, Serano-Schinken, Pilzen und Mais. Mit Käserand.", bd2, c);
+		createCatalogItem(ci2);
 		c.additem(ci);
 		c.additem(ci2);
 		
@@ -29,13 +35,16 @@ public class Test {
 		
 		BigDecimal bd3 = new BigDecimal(20.00f);
 		CatalogItem ci3 = new CatalogItem(0, "Premium", "Eine Pizza mit erlesenem Serano-Schinken, Steak, Trüffeln, Kaviar, Gorgonzola und weiteren teuren Zutaten mit Goldstaub verziert. (Mit Käserand)", bd3, c);
+		createCatalogItem(ci3);
 		c.additem(ci3);
 		
 		System.out.println(c.toString());
 		
 		//well time to buy something
 		Order o = new Order(1);
+		createOrder(o);
 		OrderItem oi = new OrderItem(0, ci3, o, "Oleksandr");
+		createOrderItem(oi);
 		o.aditem(oi);
 		
 		System.out.println(o.toString());
@@ -49,5 +58,137 @@ public class Test {
 	public static void main(String[] args) {
 		Test t = new Test();
 	}
+	
+	
+//	private static void listHoney() {
+//		Transaction tx = null;
+//		Session session = InitSessionFactory.getInstance().getCurrentSession();
+//		try {
+//			tx = session.beginTransaction();
+//			List honeys = session.createQuery("select h from Honey as h")
+//					.list();
+//			for (Iterator iter = honeys.iterator(); iter.hasNext();) {
+//				Honey element = (Honey) iter.next();
+//				log.debug(element);
+//			}
+//			tx.commit();
+//		} catch (HibernateException e) {
+//			e.printStackTrace();
+//			if (tx != null && tx.isActive())
+//				tx.rollback();
+//
+//		}
+//	}
 
+	private static void deleteCatalog(Catalog c) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.delete(c);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
+
+	private static void createCatalog(Catalog c) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.save(c);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
+	
+	private static void deleteCatalogItem(CatalogItem c) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.delete(c);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
+
+	private static void createCatalogItem(CatalogItem c) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.save(c);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
+
+	private static void deleteOrder(Order o) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.delete(o);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
+
+	private static void createOrder(Order o) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.save(o);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
+
+	private static void deleteOrderItem(OrderItem o) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.delete(o);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
+
+	private static void createOrderItem(OrderItem o) {
+		Transaction tx = null;
+		Session session = InitSessionFactory.getInstance().getCurrentSession();
+		try {
+			tx = session.beginTransaction();
+			session.save(o);
+			tx.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive())
+				tx.rollback();
+		}
+	}
 }
